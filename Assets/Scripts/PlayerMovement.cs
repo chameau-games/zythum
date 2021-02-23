56b045540ca061afc1fce3943dca5d373001aa6f
Rémi JEULIN <remifasol.j@gmail.com>
2021-02-23 12:33:18 +0100

    mouvement du joueur
    c'est le début il reste des trucs à corriger au niveau des caméras, fix au prochain commit j'espère
    
    Took 17 minutes

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Mirror;
+using UnityEngine;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    public float speed = 0.05f;
+    private CharacterController _characterController;
+
+    private void Start()
+    {
+        if (isLocalPlayer)
+        {
+            _characterController = GetComponent<CharacterController>();
+        }
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer) return;
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, 0, v);
+        _characterController.Move(dir*speed);
+    }
+}
