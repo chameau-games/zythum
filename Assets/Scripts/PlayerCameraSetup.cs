ed22e0b6abd7c9ae9801c1c43ec5e0deff8861f2
Rémi JEULIN <remifasol.j@gmail.com>
2021-02-24 23:06:47 +0100

    On peut se déplacer et contrôler la caméra à la souris !!
    reste quelques ptits trucs à fix :
    - on bouge plus vite si on va en diagonale
    et à ajouter :
    - touche echap pour mettre en pause

---
+++ Assets/Scripts/PlayerCameraSetup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Mirror;
+using UnityEngine;
+
+public class PlayerCameraSetup : NetworkBehaviour
+{
+    private Camera _mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mainCamera = Camera.main;
+        if (isLocalPlayer)
+        {
+            if (_mainCamera != null) _mainCamera.gameObject.SetActive(false);
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            GetComponent<PlayerMovement>().enabled = false;
+            GetComponentInChildren<Camera>().enabled = false;
+            GetComponentInChildren<AudioListener>().enabled = false;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (_mainCamera != null) _mainCamera.gameObject.SetActive(true);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,28 +1,41 @@
-﻿using System;
-using Mirror;
+﻿using Mirror;
 using UnityEngine;
 
 public class PlayerMovement : NetworkBehaviour
 {
-    public float speed = 0.05f;
+    public float speed;
+    public float mouseSensitivityX;
+    public float mouseSensitivityY;
     private CharacterController _characterController;
+    private Transform _transform;
+    private Camera _camera;
 
     private void Start()
     {
         if (isLocalPlayer)
         {
             _characterController = GetComponent<CharacterController>();
+            _transform = transform;
+            _camera = GetComponentInChildren<Camera>();
         }
     }
-
-    void Update()
+    private void FixedUpdate()
     {
         if (!isLocalPlayer) return;
 
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        Vector3 dir = new Vector3(h, 0, v);
-        _characterController.Move(dir*speed);
+        Vector3 movement = h * _transform.right + v * _transform.forward;
+        _characterController.Move(movement*speed);
+
+        float x = Input.GetAxis("Mouse X");
+        float y = Input.GetAxis("Mouse Y");
+
+        Vector3 yaw = new Vector3(0, x, 0);
+        transform.Rotate(yaw*mouseSensitivityX);
+
+        Vector3 pitch = new Vector3(-y, 0, 0);
+        _camera.transform.Rotate(pitch*mouseSensitivityY);
     }
 }
