9dd31564d1a80c643945eb279795a51d359af575
Nath <nathan.goetschy@epita.fr>
2021-02-25 23:23:11 +0100

    Main menu quasi prêt niveau code, début de la partie graphique

---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    public AudioMixer audiomixer;
+
+    Resolution[] resolutions;
+
+    public Dropdown resolutionDropdown;
+    public void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        
+        List<string> options = new List<string>();
+
+        int compteur = 0;
+        int currentResolutionIndex =0;
+        foreach (Resolution resolution in resolutions)
+        {
+            string option = resolution.width + "x" + resolution.height;
+            options.Add(option);
+            compteur++;
+            if (resolution.width == Screen.width && resolution.height == Screen.height)
+            {
+                currentResolutionIndex = compteur;
+            }
+        }
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+        resolutionDropdown.AddOptions(options);
+    }
+    
+    public void SetVolume(float volume)
+    {
+        audiomixer.SetFloat("Volume", volume);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+    
+}
