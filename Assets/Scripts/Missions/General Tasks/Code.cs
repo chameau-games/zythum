d53502ea54cc0cba4920a56cb164a9e47b33e7dc
Adrien GUINARD <adrienguinard69@gmail.com>
2021-06-18 04:01:58 +0200

    digicode fini enregistré dans les Preflabs

--- Assets/Scripts/Missions/General Tasks/Code.cs
+++ Assets/Scripts/Missions/General Tasks/Code.cs
@@ -1,60 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Random = System.Random;
 
 public class Code : MonoBehaviour
 {
-    public bool isLocked = false;
+    public bool isLocked = true;
     public string input = "";
-    public int iRoom = new Random().Next(20);
-    public string[] codes = {"","","",""};
+    public TMP_Text code = null;
+    public int idRoom = new Random().Next(20);
+    private string[] codes = {
+            "3726","1845","2759","2802","5889","9493","1896","7380",
+            "6187","9370","7480","4999","9908","4507","4447","1649",
+            "2526","6660","5325","7350"
+        };
     
-    //faire le digicode avec tt les game object
-    public void OnClickOneButton()
+
+    public void OnClickNumButton(string num)
     {
-        input += "1";
-    }
-    public void OnClickTwoButton()
-    {
-        input += "2";
-    }
-    public void OnClickThreeButton()
-    {
-        input += "3";
-    }
-    public void OnClickFourButton()
-    {
-        input += "4";
-    }
-    public void OnClickFiveButton()
-    {
-        input += "5";
-    }
-    public void OnClickSixButton()
-    {
-        input += "6";
-    }
-    public void OnClickSevenButton()
-    {
-        input += "7";
-    }
-    public void OnClickHeightButton()
-    {
-        input += "8";
-    }
-    public void OnClickNineButton()
-    {
-        input += "9";
+        if (input.Length < 4)
+            input += num;
+        code.text = input;
     }
     public void OnClickDelButton()
     {
         if (input.Length != 0)
             input = input.Remove(input.Length - 1);
+        code.text = input;
     }
     public void OnClickValidButton()
     {
-        
+        if (!isLocked) return;
+        if (input == codes[idRoom])
+        {
+            isLocked = false;
+        }
+        else
+        {
+            input = "";
+            code.text = input;
+        }
     }
     
     
@@ -63,13 +52,12 @@ public class Code : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        isLocked = false;
-        input = "";
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        isLocked = input == codes[iRoom];
+        
     }
 }
