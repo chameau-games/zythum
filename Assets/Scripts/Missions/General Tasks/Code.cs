5d321d0c3fbaec0b4f483c71d0a6135278fb1597
Adrien GUINARD <adrienguinard69@gmail.com>
2021-06-21 22:19:38 +0200

    pour nat

--- Assets/Scripts/Missions/General Tasks/Code.cs
+++ Assets/Scripts/Missions/General Tasks/Code.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Photon.Pun;
+using Player;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,16 +8,22 @@ using Random = System.Random;
 public class Code : MonoBehaviour
 {
     public GameObject playerCam;
+    public GameObject digicodeCam;
+    public PlayerMovement playerMov;
+    public GameObject hud;
+    public bool onCamDigi;
     public bool isLocked = true;
-    public string input = "";
-    public TMP_Text code = null;
-    public int idRoom = new Random().Next(20);
+    private string input = "";
+    public TMP_Text code;
+    private int idRoom;
+    public TMP_Text id;
     private string[] codes = {
             "3726","1845","2759","2802","5889","9493","1896","7380",
             "6187","9370","7480","4999","9908","4507","4447","1649",
             "2526","6660","5325","7350"
         };
-    
+
+
 
     public void OnClickNumButton(string num)
     {
@@ -53,12 +57,22 @@ public class Code : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        idRoom = new Random().Next(20);
+        string numDigicode = idRoom.ToString();
+        if (numDigicode.Length < 2)
+            numDigicode = '0' + numDigicode;
+        id.text = numDigicode;
+        Debug.LogError("bbb");
+        Debug.LogError(code.text);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*if (onCamDigi && Input.GetKey(KeyCode.E))
+        {
+            playerCam.SetActive(true);
+            playerMov.enabled = true;
+        }*/
     }
 }
--- Assets/Scripts/Player/PlayerActions.cs
+++ Assets/Scripts/Player/PlayerActions.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 
 namespace Player
 {
-    public class PlayerActions : MonoBehaviour
+    public class PlayerActions : MonoBehaviourPun
     {
+        public GameObject playerCamera;
+        public PlayerMovement playerMovement;
+        
         private int code;
         public float maxReach;
         private HUD hud;
@@ -18,6 +21,8 @@ namespace Player
 
         private void Start()
         {
+            if (!photonView.IsMine)
+                enabled = false;
             hud = GameObject.Find("HUD").GetComponent<HUD>();
             spawnpointSdc = GameObject.Find("Spawnpoint salle de contrôle").transform;
             aerationVent = GameObject.Find("aerationVent").transform;
@@ -36,26 +41,64 @@ namespace Player
                 //ça touche la bouche d'aération (pour passer dans la salle de controle & c'est le joueur qui peut y aller
                 if (PhotonNetwork.IsMasterClient && hit.transform == aerationVent)
                 {
-                    if (Input.GetKey(KeyCode.U))
+                    if (Input.GetKey(KeyCode.E))
                     {
                         transform.SetPositionAndRotation(spawnpointSdc.position, spawnpointSdc.rotation);
                     }
                     else
                     {
-                        hud.SetInformationText("Appuie sur U pour passer dans la bouche d'aération");
+                        hud.SetInformationText("Appuie sur E pour passer dans la bouche d'aération");
                         hud.ShowText();
                     }
                 }
                 else if (PhotonNetwork.IsMasterClient &&
                          hit.transform == gameManager.boutonQuiOuvreLaPorteDeLaCellule.transform)
                 {
-                    if (Input.GetKey(KeyCode.U))
+                    if (Input.GetKey(KeyCode.E))
                     {
                         gameManager.OuvrirGrille();
                     }
                     else
                     {
-                        hud.SetInformationText("Appuie sur U pour ouvrir la porte de la cellule de l'infiltré");
+                        hud.SetInformationText("Appuie sur E pour ouvrir la porte de la cellule de l'infiltré");
+                        hud.ShowText();
+                    }
+                }
+                else if (hit.transform.gameObject.CompareTag("digicode"))
+                {
+                    if (Input.GetKey(KeyCode.E))
+                    {
+                        Code code = hit.transform.parent.Find("CanvasDigi").GetComponent<Code>();
+                        code.playerCam = playerCamera;
+                        code.playerMov = playerMovement;
+                        code.hud = hud.gameObject;
+                        code.digicodeCam.SetActive(true);
+                        hud.gameObject.SetActive(false);
+                        playerCamera.SetActive(false);
+                        playerMovement.enabled = false;
+                        code.onCamDigi = true;
+                        //GameObject.Find("MissionList").GetComponent<MissionList>().TaskServer();
+                    }
+                    else
+                    {
+                        hud.SetInformationText("Appuie sur E pour regarder de plus près le digicode");
+                        hud.ShowText();
+                    }
+                }
+                else if (hit.transform.gameObject.CompareTag("serverTag"))
+                {
+                    if (Input.GetKey(KeyCode.E))
+                    {
+                        ServerTask serverTask = hit.transform.parent.GetComponent<ServerTask>();
+                        serverTask.playerCam = playerCamera;
+                        serverTask.playerMov = playerMovement;
+                        playerCamera.SetActive(false);
+                        playerMovement.enabled = false;
+                        hit.transform.parent.Find("Camera Serveur").gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        hud.SetInformationText("Appuie sur E pour réparer le rack");
                         hud.ShowText();
                     }
                 }
